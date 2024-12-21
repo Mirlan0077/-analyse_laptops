489585c99b1668e5489cc4032a3e2cefe43b0ca0
Mirlan0077 <mirlan.0077m@gmail.com>
2024-12-22 00:50:31 +0100

    Missbrauchte Path wurde gelöscht

--- laptop_app/urls.py
+++ laptop_app/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_csv_and_analyze, name='upload_csv_and_analyze'),
-    path('analyze/', views.analyze_laptops, name='analyze_laptops'),
 ]
