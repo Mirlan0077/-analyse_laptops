4426bc1c615e3f63233646ddbeaf15276ef6a9d3
Mirlan0077 <mirlan.0077m@gmail.com>
2024-12-21 17:07:28 +0100

    verbessert

--- laptop_app/views.py
+++ laptop_app/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from .models import Laptop
 from .utils.csv_processor import process_csv
 
+
 def analyze_laptops(request):
 
     top_10_laptops = Laptop.objects.order_by('-rating')[:10]
@@ -29,9 +30,7 @@ def upload_csv_and_analyze(request):
 
     file_path = 'laptop_app/fixtures/mindfactory_done.csv'
     process_csv(file_path)
-
     valid_laptops = Laptop.objects.filter(rating__gt=0).order_by('-rating')
-
     top_10_laptops = valid_laptops[:10]
     low_10_laptops = valid_laptops.reverse()[:10]
 
